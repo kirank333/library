76ed546b611de135938f2702bfdd47318f89fb7d
kirank333 <kirank.20cse@kongu.edu>
2023-05-08 17:50:16 +0530

    first commit

---
+++ filter.js
@@ -0,0 +1,270 @@
+let products = {
+    data: [{
+            productName: "Spider-Man/Venom -Donny Cates",
+            category: "Comics",
+            price: "Rs.250",
+            image: "https://m.media-amazon.com/images/P/B08CY8TGDP.01._SCLZZZZZZZ_SX500_.jpg",
+            
+        },
+        {
+            productName: "Avengers-Gerry dugan",
+            category: "Comics",
+            price: "Rs.350",
+            image: "https://m.media-amazon.com/images/I/91cyJhyr6CL._AC_UL600_FMwebp_QL65_.jpg",
+            
+        },
+        {
+            productName: "Stranger things-Michael moreci",
+            category: "Comics",
+            price:"Rs.290",
+            image: "https://m.media-amazon.com/images/I/91nDKosx77L._AC_UL600_FMwebp_QL65_.jpg",
+    
+        },
+        {
+            productName: "Dawn of dc-Michael moreci",
+            category: "Comics",
+            price:"Rs.360",
+            image: "https://m.media-amazon.com/images/I/91PuOcLUjHL._AC_UL600_FMwebp_QL65_.jpg",
+            
+        },
+        {
+            productName: "Destination Moon",
+            category: "Comics",
+            price: "Rs.350",
+            image: "https://m.media-amazon.com/images/I/81HLWQJlWNL._AC_UL600_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "The adventure of sherlock homes",
+            category: "Comics",
+            price: "Rs.350",
+            image: "https://m.media-amazon.com/images/I/81IQKewTk4L._AC_UL600_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "Pokemon",
+            category: "Comics",
+            price: "Rs.350",
+            image: "https://m.media-amazon.com/images/I/91FiWNDhdWL._AC_UL600_FMwebp_QL65_.jpg",
+        },
+
+        {
+            productName: "Tom and jerry",
+            category: "Comics",
+            price: "Rs.450",
+            image: "https://m.media-amazon.com/images/I/71mwOpKXHsL._AC_UL600_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "Dracula",
+            category: "Horror",
+            price: "Rs.500",
+            image: "https://m.media-amazon.com/images/I/81q5xK4+dqS._AC_UL600_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "Ghosts in the dark silence",
+            category: "Horror",
+            price: "Rs.500",
+            image: "https://m.media-amazon.com/images/I/51ilPjmFjeL._SX198_BO1,204,203,200_QL40_FMwebp_.jpg",
+        },
+        {
+            productName: "that Night",
+            category: "Horror",
+            price: "Rs.500",
+            image: "https://m.media-amazon.com/images/I/81b1PP4RK1L._AC_UL600_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "Horror stories",
+            category: "Horror",
+            price: "Rs.500",
+            image: "https://m.media-amazon.com/images/I/819n0tHLQAL._AC_UL600_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "Darkness",
+            category: "Horror",
+            price: "Rs.500",
+            image: "https://m.media-amazon.com/images/I/715bCq4MlEL._AC_UL600_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "Steve Jobs",
+            category: "Biography",
+            price: "Rs.500",
+            image: "https://m.media-amazon.com/images/I/41n1edvVlLL._AC_UL600_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "Will",
+            category: "Biography",
+            price: "Rs.129",
+            image: "https://m.media-amazon.com/images/I/91C0eiPBxLL._AC_UL600_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "Worlds greatest leaders",
+            category: "Biography",
+            price: "Rs.200",
+            image: "https://m.media-amazon.com/images/I/71aLHan2nIL._AC_UL600_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "Indira Gandhi",
+            category: "Biography",
+            price: "Rs.250",
+            image: "https://m.media-amazon.com/images/I/816DY2WjglL._AC_UL600_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "Swami Viviekanada",
+            category: "Biography",
+            price: "Rs.300",
+            image: "https://m.media-amazon.com/images/I/8134Hol8MYL._AC_UL600_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "Dr.Ambedkar",
+            category: "Biography",
+            price: "Rs.450",
+            image: "https://m.media-amazon.com/images/I/71s3dJKSuCL._AC_UL600_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "Agile Project",
+            category: "Management",
+            price: "Rs.500",
+            image: "https://m.media-amazon.com/images/I/713nQQQPfzL._AC_UY327_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "Business Management",
+            category: "Management",
+            price: "Rs.500",
+            image: "https://m.media-amazon.com/images/I/81Sod2XyNgL._AC_UL600_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "Project Management",
+            category: "Management",
+            price: "Rs.500",
+            image: "https://m.media-amazon.com/images/I/61DCXbTCVvL._AC_UL600_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "Time Management",
+            category: "Management",
+            price: "Rs.500",
+            image: "https://m.media-amazon.com/images/I/41EnSTe9jZL._AC_UL600_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "Inventory Management",
+            category: "Management",
+            price: "Rs.500",
+            image: "https://m.media-amazon.com/images/I/419fWrhPctL._AC_UL600_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "Human Resource",
+            category: "Management",
+            price: "Rs.500",
+            image: "https://m.media-amazon.com/images/I/81lFDCOcwTL._AC_UL600_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "Warehouse Management",
+            category: "Management",
+            price: "Rs.500",
+            image: "https://m.media-amazon.com/images/I/81rrgfSMHCL._AC_UY327_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "Marketing Management",
+            category: "Management",
+            price: "Rs.550",
+            image: "https://m.media-amazon.com/images/I/81kAuBin8BL._AC_UL600_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "APJ Abdul Kalam",
+            category: "Biography",
+            price: "Rs.499",
+            image: "https://m.media-amazon.com/images/I/71KKZlVjbwL._AC_UL600_FMwebp_QL65_.jpg",
+        },
+        {
+            productName: "Elon Musk",
+            category: "Biography",
+            price: "Rs.499",
+            image: "https://m.media-amazon.com/images/I/419vzujHBtL._AC_UL600_FMwebp_QL65_.jpg",
+        },
+    ],
+};
+
+for (let i of products.data) {
+    //Create Card
+    let card = document.createElement("div");
+    //Card should have category and should stay hidden initially
+    card.classList.add("card", i.category, "hide");
+    //image div
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+    //img tag
+    let image = document.createElement("img");
+    image.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+    //container
+    let container = document.createElement("div");
+    container.classList.add("container");
+    //product name
+    let name = document.createElement("h5");
+    name.classList.add("product-name");
+    name.innerText = i.productName.toUpperCase();
+    container.appendChild(name);
+    //price
+    let price = document.createElement("h6");
+    price.innerText = "" + i.price;
+    container.appendChild(price);
+
+    card.appendChild(container);
+    document.getElementById("products").appendChild(card);
+}
+
+//parameter passed from button (Parameter same as category)
+function filterProduct(value) {
+    //Button class code
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+        //check if value equals innerText
+        if (value.toUpperCase() == button.innerText.toUpperCase()) {
+            button.classList.add("active");
+        } else {
+            button.classList.remove("active");
+        }
+    });
+
+    //select all cards
+    let elements = document.querySelectorAll(".card");
+    //loop through all cards
+    elements.forEach((element) => {
+        //display all cards on 'all' button click
+        if (value == "all") {
+            element.classList.remove("hide");
+        } else {
+            //Check if element contains category class
+            if (element.classList.contains(value)) {
+                //display element based on category
+                element.classList.remove("hide");
+            } else {
+                //hide other elements
+                element.classList.add("hide");
+            }
+        }
+    });
+}
+
+//Search button click
+document.getElementById("search").addEventListener("click", () => {
+    //initializations
+    let searchInput = document.getElementById("search-input").value;
+    let elements = document.querySelectorAll(".product-name");
+    let cards = document.querySelectorAll(".card");
+
+    //loop through all elements
+    elements.forEach((element, index) => {
+        //check if text includes the search value
+        if (element.innerText.includes(searchInput.toUpperCase())) {
+            //display matching card
+            cards[index].classList.remove("hide");
+        } else {
+            //hide others
+            cards[index].classList.add("hide");
+        }
+    });
+});
+
+//Initially display all products
+window.onload = () => {
+    filterProduct("all");
+};
